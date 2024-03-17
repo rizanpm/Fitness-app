2fa27fce9fd66db648683ce3cfbe02d9b0c815f7
rizanpm <rizanrafeeque786@gmail.com>
2024-03-17 14:26:42 +0530

    added new files

---
+++ src/Components/Join/Join.jsx
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react'
+import './Join.css'
+import emailjs from '@emailjs/browser'
+
+const Join = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_7cmgfwm', 'template_qzyxduk', form.current, {
+        publicKey: 'lMHvIQzHqiyP3B4em',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+  return (
+    <div className="Join" id="join-us">
+        <div className="left-j">
+          <hr />
+            <div>
+                <span className='stroke-text'>READY TO</span>
+                <span>LEVEL UP</span>
+            </div>
+            <div>
+                <span>YOUR BODY</span>
+                <span className='stroke-text'>WITH US?</span>
+            </div>
+        </div>
+        <div className="right-j">
+          <form ref={form} className="email-container" onSubmit={sendEmail}>
+            <input type='email' name='user_email' placeholder='Enter your Email address' />
+            <button className='btn btn-j'>Join now</button>
+          </form>
+        </div>
+    </div>
+  )
+}
+
+export default Join
---
+++ src/data/testimonialsData.js
@@ -0,0 +1,27 @@
+import image1 from "../assets/t-image1.png";
+import image2 from "../assets/t-image2.jpg";
+import image3 from "../assets/t-image3.jpg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
+    name: 'MATHEW HENDRICKSON',
+    status : 'ENTREPRENEUR'
+  },
+  {
+    image: image2,
+    review: 
+    "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle",
+    name: 'JOHN KEVIN',
+    status: 'COACH'
+  },
+  {
+    image : image3,
+    review:
+    "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
+    name: 'FRANKLIN',
+    status: "CUSTOMER"
+  }
+];
